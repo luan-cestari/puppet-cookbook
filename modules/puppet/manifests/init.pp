113eb945584e804eee9c31bc5841943ae463e5ce
HiS <hinrich.sager@gmail.com>
2013-12-08 16:19:38 +0000

    cron for update by git co

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -3,4 +3,15 @@ class puppet {
     source => 'puppet:///modules/puppet/papply.sh',
     mode   => '0755',
   }
+  file { '/usr/local/bin/pull-updates':
+    source => 'puppet:///modules/puppet/pull-updates.sh',
+    mode   => '0755',
+  }
+  cron { 'run-puppet':
+    ensure  => 'present',
+    user    => 'vagrant',
+    command => '/usr/local/bin/pull-updates',
+    minute  => '*/10',
+    hour    => '*',
+  }
 }
