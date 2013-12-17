6a15dc818d2dc6747ccb0396712463fffceefaad
HiS <hinrich.sager@gmail.com>
2013-12-17 09:35:26 +0100

    comment priv ssh key setting

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -7,11 +7,11 @@ class puppet {
     source => 'puppet:///modules/puppet/pull-updates.sh',
     mode   => '0755',
   }
-  file { '/home/deploy/.ssh/id_rsa':
-    source => 'puppet:///modules/puppet/deploy.priv',
-    owner  => 'deploy',
-    mode   => '0600',
-  }
+  #  file { '/home/deploy/.ssh/id_rsa':
+  #  source => 'puppet:///modules/puppet/deploy.priv',
+  #  owner  => 'deploy',
+  #  mode   => '0600',
+  #}
   file { '/home/deploy/.ssh/id_rsa.pub':
     source => 'puppet:///modules/puppet/deploy.pub',
     owner  => 'deploy',
