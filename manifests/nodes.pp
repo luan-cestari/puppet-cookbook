bc32922db931ee332dd9a2ecdf7b241c5ef554ba
HiS <hinrich.sager@gmail.com>
2013-12-19 10:34:38 +0100

    using tagged for ntp in node config

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -2,3 +2,14 @@ node 'puppet-cookbook' {
   include puppet
   include memcached
 }
+
+node 'puppet-cookbook' {
+  include admin::ntp
+
+  if tagged('admin::ntp') {
+    notify { 'This node is running NTP': }
+  }
+  if tagged('admin') {
+    notify { 'This node includes at least one class from the admin module': }
+  }
+}
