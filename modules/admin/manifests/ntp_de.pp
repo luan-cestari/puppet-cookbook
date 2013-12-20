ac9d3c707605ebfe155f1620869c1d1c6852f982
HiS <hinrich.sager@gmail.com>
2013-12-20 09:12:01 +0100

    fix ntp_de source value

--- modules/admin/manifests/ntp_de.pp
+++ modules/admin/manifests/ntp_de.pp
@@ -1,5 +1,5 @@
 class admin::ntp_de inherits admin::ntp {
   File['/etc/ntpd.conf'] {
-    source => 'puppet://modules/admin/ntp_de.conf',
+    source => 'puppet:///modules/admin/ntp_de.conf',
   }
 }
