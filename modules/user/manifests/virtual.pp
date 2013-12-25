d2a56ae0197140e06a39d5628fa91a7e9067066d
HiS <hinrich.sager@gmail.com>
2013-12-25 21:19:50 +0100

    WIP ssh user

--- modules/user/manifests/virtual.pp
+++ modules/user/manifests/virtual.pp
@@ -1,4 +1,25 @@
 class user::virtual {
+  define ssh_user($key) {
+    user { $name:
+      ensure     => present,
+      managehome => true,
+    }
+
+    file { "/home/${name}/.ssh":
+      ensure => directory,
+      mode   => '0700',
+      owner  => $name,
+    }
+
+    ssh_authorized_key { "${name}_key":
+      key     => $key,
+      type    => 'ssh-rsa',
+      user    => $name,
+      require => File["/home/${name}/.ssh"],
+    }
+  }
   @user { 'admin': ensure => present }
-  @user { 'deploy': ensure => present }
+  @user { 'deploy':
+    ensure => present
+  }
 }
